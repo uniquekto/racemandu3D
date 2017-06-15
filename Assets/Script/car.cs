ea40f1e11154ee75badfcad9c5782c0464d4090d
pramesh <pramesh_morang@hotmail.com>
2017-06-15 14:12:00 +0300

    cube can be moved with the arrow keys.

---
+++ Assets/Script/car.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour {
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 10f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+	}
+}
